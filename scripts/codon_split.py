9cc9e8953ab8155071670baf62f06d3695ad7715
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-09-16 11:56:03 +0300

    fixed codon_split.py

--- scripts/codon_split.py
+++ scripts/codon_split.py
@@ -7,11 +7,12 @@ def kmer_extraction(seq, ksize):
     with open(seq) as genome:
         first_line = genome.readline()
         lines = genome.read().split('\n')
-        lines=''.join(lines)
+    lines=''.join(lines)
 
     
     n_kemrs = len(lines)-ksize+1
     with open('codon_seq.fa', 'w') as c:
-        for i in range(n_kmers):                                                                                                    c.write(lines[i:i+ksize])
+        for i in range(n_kmers):
+            c.write(lines[i:i+ksize])
 
 
